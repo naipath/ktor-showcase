f1959a2d04b979a4a0dfa676e98614ba2be15401
tvs <teije-vs@hotmail.com>
2020-06-09 21:21:11 +0200

    Remove webjars and ordered dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.Coroutines
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 val logback_version: String by project
 val ktor_version: String by project
 val kotlin_version: String by project
@@ -26,20 +23,30 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+
     implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    // Testing
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+
+    // Additional features
+
+    // HTML + CSS dsl
     implementation("io.ktor:ktor-html-builder:$ktor_version")
     implementation("org.jetbrains:kotlin-css-jvm:1.0.0-pre.31-kotlin-1.2.41")
-    implementation("io.ktor:ktor-server-host-common:$ktor_version")
+
+    // JSON
     implementation("io.ktor:ktor-gson:$ktor_version")
+
+    // Experimental
     implementation("io.ktor:ktor-locations:$ktor_version")
     implementation("io.ktor:ktor-metrics:$ktor_version")
     implementation("io.ktor:ktor-server-sessions:$ktor_version")
-    implementation("io.ktor:ktor-webjars:$ktor_version")
-    implementation("org.webjars:jquery:3.2.1")
     implementation("io.ktor:ktor-websockets:$ktor_version")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
